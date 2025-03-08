133ae2af18141cfcb02c04757b0719aa5aa85db0
Riyan Fanani <ogickarecka1108@gmail.com>
2025-03-08 07:10:44 +0700

    update: implement fluid-player

---
+++ src/lib/components/VideoPlayer.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import fluidPlayer from 'fluid-player'
+	import 'fluid-player/src/css/fluidplayer.css'
+	import { onMount } from 'svelte'
+
+	interface QualityInterface {
+		id: number
+		url: string
+		resolution: string
+		bitrate: number
+		videoAssetId: number | null
+	}
+
+	interface VideoInterface {
+		id: number
+		name: string
+		description: string | null
+		url: string
+		format: string
+		thumbnail: string
+		views: number
+		qualities: QualityInterface[]
+	}
+
+	let { video }: { video: VideoInterface } = $props()
+
+	onMount(() => {
+		fluidPlayer('player', {
+			hls: {
+				overrideNative: true
+			}
+		})
+	})
+</script>
+
+<div class="h-[400px]">
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video id="player" width="600">
+		<source src={video.url} type="application/x-mpegURL" />
+	</video>
+</div>
--- src/routes/stream/+page.svelte
+++ src/routes/stream/+page.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
+	import VideoPlayer from '$lib/components/VideoPlayer.svelte'
 	import type { PageData } from './$types'
 
 	let { data }: { data: PageData } = $props()
-
-	onMount(() => {
-		const video = document.getElementById('video') as HTMLVideoElement
-
-		if (video.canPlayType('application/vnd.apple.mpegurl')) {
-			video.src = String(data.video?.url)
-			// @ts-ignore
-		} else if (Hls.isSupported()) {
-			// @ts-ignore
-			var hls = new Hls()
-			hls.loadSource(String(data.video?.url))
-			hls.attachMedia(video)
-		}
-	})
 </script>
 
-<!-- svelte-ignore a11y_media_has_caption -->
-<video id="video" controls preload="none"></video>
+{#if !data.video}
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+{:else}
+	<VideoPlayer video={data.video} />
+{/if}
