7e9d5b697af5d1e15ea0268808c73dbe50cc547f
Riyan Fanani <ogickarecka1108@gmail.com>
2025-05-20 23:20:51 +0700

    add: admin layout

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,1 +1,8 @@
-<h1>aa11</h1>
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+	import type { LayoutData } from './$types'
+
+	let { data, children }: { data: LayoutData; children: Snippet } = $props()
+</script>
+
+{@render children()}
