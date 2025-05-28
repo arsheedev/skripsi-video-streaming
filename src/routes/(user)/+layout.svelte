8925759a32319e8311d0e757b8adfc9413353258
Riyan Fanani <ogickarecka1108@gmail.com>
2025-05-28 18:56:14 +0700

    update: user layout

--- src/routes/(user)/+layout.svelte
+++ src/routes/(user)/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button'
 	import { Search } from 'lucide-svelte'
 	import { Toaster } from 'svelte-sonner'
 	import '../../app.css'
@@ -19,7 +18,9 @@
 		class="sticky top-0 z-50 border-b border-gray-700/50 bg-gray-800/80 px-8 py-4 shadow-lg backdrop-blur-md"
 	>
 		<div class="container mx-auto flex items-center justify-between">
-			<h1 class="text-3xl font-bold text-[#E50914]">ZAPPLE PLAY</h1>
+			<a href="/">
+				<h1 class="text-3xl font-bold text-[#E50914]">ZAPPLE PLAY</h1>
+			</a>
 
 			<div class="absolute left-1/2 w-1/3 min-w-[300px] -translate-x-1/2 transform">
 				<div class="relative">
@@ -31,13 +32,6 @@
 					<Search class="absolute left-3 top-2.5 text-gray-400" size={20} />
 				</div>
 			</div>
-
-			<Button
-				href="/auth/login"
-				class="rounded-md bg-[#E50914] px-6 py-2 font-medium text-white transition hover:bg-[#f6121d]"
-			>
-				Login
-			</Button>
 		</div>
 	</nav>
 
