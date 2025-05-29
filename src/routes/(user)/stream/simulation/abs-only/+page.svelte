53d5851582b4059ea4c55f4a109d5453b4bcdfaf
Riyan Fanani <ogickarecka1108@gmail.com>
2025-05-30 06:41:30 +0700

    add: abs only simulation page

---
+++ src/routes/(user)/stream/simulation/abs-only/+page.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	import { browser } from '$app/environment'
+	import { page } from '$app/state'
+	import Hls from 'hls.js'
+	import type { PageData } from './$types'
+
+	let { data }: { data: PageData } = $props()
+	let hls: Hls
+	let count = $state(0)
+	const downloadSpeed = page.url.searchParams.get('speed')?.split(',') || [5000]
+
+	function videoPlayer(node: HTMLVideoElement) {
+		if (!data.video?.url) return
+
+		hls = new Hls({ maxBufferLength: 5, maxMaxBufferLength: 10 })
+
+		hls.loadSource(data.video.url)
+		hls.attachMedia(node)
+
+		const interval = setInterval(() => {
+			if (Number(downloadSpeed[count]) > 341) {
+				hls.nextLevel = 2
+				count++
+			} else if (Number(downloadSpeed[count]) > 170) {
+				hls.nextLevel = 1
+				count++
+			} else {
+				hls.nextLevel = 0
+				count++
+			}
+
+			if (count === downloadSpeed.length) count = 0
+		}, 2000)
+	}
+</script>
+
+{#if !data.video}
+	<pre>{JSON.stringify(data, null, 2)}</pre>
+{:else}
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video
+		id="player"
+		width="600"
+		preload="metadata"
+		poster={data.video.thumbnail}
+		controls
+		use:videoPlayer
+	>
+		<source type="application/x-mpegURL" />
+	</video>
+	{#if browser}
+		<button onclick={() => (hls.nextLevel = -1)}>auto</button>
+		<button onclick={() => (hls.nextLevel = 0)}>240p</button>
+		<button onclick={() => (hls.nextLevel = 1)}>480p</button>
+		<button onclick={() => (hls.nextLevel = 2)}>720p</button>
+
+		{#if Number(downloadSpeed[count]) > 341}
+			<p>quality: 720p</p>
+		{:else if Number(downloadSpeed[count]) > 170}
+			<p>quality: 480p</p>
+		{:else}
+			<p>quality: 240p</p>
+		{/if}
+		<p>speed: {downloadSpeed[count]}</p>
+	{/if}
+{/if}
