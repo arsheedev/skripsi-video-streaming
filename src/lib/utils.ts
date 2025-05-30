48b17c1092873156e986eb6639149bbbdf61ab35
Riyan Fanani <ogickarecka1108@gmail.com>
2025-05-30 09:50:05 +0700

    delete: unnecessary libs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,47 +1,8 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-interface ResponseInterface {
-	results:
-		| {
-				name: {
-					first: string
-					last: string
-				}
-				email: string
-				login: {
-					username: string
-				}
-				picture: {
-					large: string
-					medium: string
-					thumbnail: string
-				}
-		  }[]
-		| null
-	error: string | null
-}
-
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
-}
-
-export async function getUser() {
-	const fetchData = await fetch('https://randomuser.me/api?nat=us')
-	const res: ResponseInterface = await fetchData.json()
-
-	if (res.results && !res.error) {
-		console.log('test')
-		return {
-			username: res.results[0].login.username,
-			name: res.results[0].name.first + ' ' + res.results[0].name.last,
-			imageUrl: res.results[0].picture.thumbnail
-		}
-	}
-
-	const user = 'User' + Math.floor(Math.random() * (1000 - 1 + 1)) + 1
-
-	return { username: user, name: user, imageUrl: '/profile.png' }
 }
 
 export function loadVideo() {
