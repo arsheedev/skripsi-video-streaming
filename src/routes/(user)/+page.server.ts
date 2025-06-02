ec3d92107523d07192abef310821cc064e53726b
Riyan Fanani <ogickarecka1108@gmail.com>
2025-06-02 09:59:05 +0700

    update: sort video by newest first

--- src/routes/(user)/+page.server.ts
+++ src/routes/(user)/+page.server.ts
@@ -2,7 +2,10 @@ import db from '$lib/server/db'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
-	const videos = await db.videoAsset.findMany({ include: { comments: true } })
+	const videos = await db.videoAsset.findMany({
+		include: { comments: true },
+		orderBy: { createdAt: 'desc' }
+	})
 
 	return { videos }
 }
